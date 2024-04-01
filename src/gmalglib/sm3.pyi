dc3385bd2cf1e52d1a50395311b54b638bbf76fc
ww-rm <ww-rm@qq.com>
2024-04-01 18:51:13 +0800

    add gmalglib.sm3

---
+++ src/gmalglib/sm3.pyi
@@ -0,0 +1,36 @@
+SM3_MAX_MSG_BITLEN: int
+
+
+class SM3:
+    """SM3 Algorithm."""
+
+    def __init__(self, data: bytes = b"") -> None:
+        """SM3 Algorithm.
+
+        Args:
+            data: Initial data.
+        """
+
+    def update(self, data: bytes) -> None:
+        """Update state.
+
+        Args:
+            data: Bytes data.
+        """
+
+    def digest(self) -> bytes:
+        """Get digest.
+
+        Returns:
+            digest: Digest in bytes.
+        """
+
+    def reset(self) -> None:
+        """Reset internal state."""
+
+    def copy(self) -> SM3:
+        """Copy a new SM3 object with identical state.
+
+        Returns:
+            sm3_obj: A new SM3 object.
+        """
