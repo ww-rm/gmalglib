6b666c9422a1de27f5732998d4ce2e2f42285f67
ww-rm <ww-rm@qq.com>
2024-04-03 21:24:41 +0800

    add TestSM4

--- tests.py
+++ tests.py
@@ -65,5 +65,34 @@ class TestSM3(unittest.TestCase):
             self.assertEqual(sm31.digest(), sm32.digest())
 
 
+class TestSM4(unittest.TestCase):
+    def setUp(self) -> None:
+        self.c = gmalglib.sm4.SM4(bytes.fromhex("0123456789ABCDEFFEDCBA9876543210"))
+
+    def test_case1(self):
+        cipher = self.c.encrypt(bytes.fromhex("0123456789ABCDEFFEDCBA9876543210"))
+        self.assertEqual(cipher, bytes.fromhex("681edf34d206965e86b3e94f536e4246"))
+
+        plain = self.c.decrypt(cipher)
+        self.assertEqual(plain, bytes.fromhex("0123456789ABCDEFFEDCBA9876543210"))
+
+    def test_case2(self):
+        cipher = self.c.encrypt(bytes.fromhex("0123456789ABCDEFFEDCBA9876543210"))
+        for _ in range(999999):
+            cipher = self.c.encrypt(cipher)
+        self.assertEqual(cipher, bytes.fromhex("595298c7c6fd271f0402f804c33d3f66"))
+
+        plain = self.c.decrypt(cipher)
+        for _ in range(999999):
+            plain = self.c.decrypt(plain)
+        self.assertEqual(plain, bytes.fromhex("0123456789ABCDEFFEDCBA9876543210"))
+
+    def test_raises(self):
+        self.assertRaises(ValueError, self.c.encrypt, b"123456781234567")
+        self.assertRaises(ValueError, self.c.encrypt, b"12345678123456781")
+        self.assertRaises(ValueError, self.c.decrypt, b"123456781234567")
+        self.assertRaises(ValueError, self.c.decrypt, b"12345678123456781")
+
+
 if __name__ == "__main__":
     unittest.main()
