34c0cbb4a860a578c16f1ed9ed4f27bcf572af21
ww-rm <ww-rm@qq.com>
2024-04-12 01:07:57 +0800

    add sm2 files

--- setup.py
+++ setup.py
@@ -7,6 +7,10 @@ core_module = Extension(
         "src/gmalglib/core/sm3.c",
         "src/gmalglib/core/sm4.c",
         "src/gmalglib/core/zuc.c",
+        "src/gmalglib/core/bignum.c",
+        "src/gmalglib/core/random.c",
+        "src/gmalglib/core/sm2curve.c",
+        "src/gmalglib/core/sm2.c",
     ],
     include_dirs=[
         "include",
@@ -16,6 +20,10 @@ core_module = Extension(
         "include/gmalglib/sm3.h",
         "include/gmalglib/sm4.h",
         "include/gmalglib/zuc.h",
+        "include/gmalglib/bignum.h",
+        "include/gmalglib/random.h",
+        "include/gmalglib/sm2curve.h",
+        "include/gmalglib/sm2.h",
     ]
 )
 
