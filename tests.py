8320ae5fc35e7636c8982f0f7b51e2c332b6420c
ww-rm <ww-rm@qq.com>
2024-04-14 12:27:11 +0800

    add SM2 encrypt tests

--- tests.py
+++ tests.py
@@ -124,6 +124,7 @@ class TestSM2(unittest.TestCase):
         self.assertEqual(alg.SM2().generate_pk(d), lib.sm2.SM2.get_pk(d))
 
         d, _ = lib.sm2.SM2().generate_keypair()
+        d = b"\x00" * (32 - len(d)) + d
         self.assertEqual(alg.SM2().generate_pk(d), lib.sm2.SM2.get_pk(d))
 
     def test_sign1(self):
@@ -159,18 +160,19 @@ class TestSM2(unittest.TestCase):
 
     def test_sign4(self):
         sk, pk = alg.SM2().generate_keypair()
+        sk = b"\x00" * (32 - len(sk)) + sk
 
         algsm2 = alg.SM2(sk, b"test_sign4", pk)
         libsm2 = lib.sm2.SM2(sk, pk, b"test_sign4")
 
         msg = b"12345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678"
-        for _ in range(100):
+        for _ in range(10):
             r, s = algsm2.sign(msg)
             r = b"\x00" * (32 - len(r)) + r
             s = b"\x00" * (32 - len(s)) + s
             self.assertTrue(libsm2.verify(msg, r + s))
 
-        for _ in range(100):
+        for _ in range(10):
             s = libsm2.sign(msg)
             self.assertTrue(algsm2.verify(msg, s[:32], s[32:]))
 
@@ -179,8 +181,66 @@ class TestSM2(unittest.TestCase):
         libsm2 = lib.sm2.SM2(sk, pk, b"test_sign5")
         msg = b"fast sign!fast sign!fast sign!fast sign!fast sign!fast sign!fast sign!fast sign!fast sign!fast sign!fast sign!"
 
-        for _ in range(1000):
+        for _ in range(10):
             self.assertTrue(libsm2.verify(msg, libsm2.sign(msg)))
+
+    def test_encrypt1(self):
+        sm2 = lib.sm2.SM2(
+            bytes.fromhex("3945208F 7B2144B1 3F36E38A C6D39F95 88939369 2860B51A 42FB81EF 4DF7C5B8"),
+            bytes.fromhex("04"
+                          "09F9DF31 1E5421A1 50DD7D16 1E4BC5C6 72179FAD 1833FC07 6BB08FF3 56F35020"
+                          "CCEA490C E26775A5 2DC6EA71 8CC1AA60 0AED05FB F35E084A 6632F607 2DA9AD13"),
+            rnd_fn=lambda _: 0x59276E27_D506861A_16680F3A_D9C02DCC_EF3CC1FA_3CDBE4CE_6D54B80D_EAC1BC21.to_bytes(32, "little"),
+        )
+
+        cipher = sm2.encrypt(b"encryption standard")
+        self.assertEqual(cipher, bytes.fromhex("04"
+                                               "04EBFC71 8E8D1798 62043226 8E77FEB6 415E2EDE 0E073C0F 4F640ECD 2E149A73"
+                                               "E858F9D8 1E5430A5 7B36DAAB 8F950A3C 64E6EE6A 63094D99 283AFF76 7E124DF0"
+                                               "59983C18 F809E262 923C53AE C295D303 83B54E39 D609D160 AFCB1908 D0BD8766"
+                                               "21886C A989CA9C 7D580873 07CA9309 2D651EFA"))
+
+        self.assertEqual(sm2.decrypt(cipher), b"encryption standard")
+
+    def test_encrypt2(self):
+        d, pk = lib.sm2.SM2().generate_keypair()
+        plain = b"random SM2 encrypt test random SM2 encrypt test random SM2 encrypt test random SM2 encrypt test random SM2 encrypt test random SM2 encrypt test random SM2 encrypt test"
+
+        sm2 = lib.sm2.SM2(d, pk=pk)
+        cipher = sm2.encrypt(plain)
+        self.assertEqual(sm2.decrypt(cipher), plain)
+
+        sm2 = lib.sm2.SM2(d, pk=pk, pc_mode=lib.sm2.SM2_PCMODE_COMPRESS)
+        cipher = sm2.encrypt(plain)
+        self.assertEqual(sm2.decrypt(cipher), plain)
+
+        sm2 = lib.sm2.SM2(d, pk=pk, pc_mode=lib.sm2.SM2_PCMODE_MIX)
+        cipher = sm2.encrypt(plain)
+        self.assertEqual(sm2.decrypt(cipher), plain)
+
+    def test_encrypt3(self):
+        sk, pk = alg.SM2().generate_keypair()
+        sk = b"\x00" * (32 - len(sk)) + sk
+
+        algsm2 = alg.SM2(sk, b"test_encrypt3", pk, pc_mode=alg.PC_MODE.RAW)
+        libsm2 = lib.sm2.SM2(sk, pk, b"test_encrypt3", pc_mode=lib.sm2.SM2_PCMODE_COMPRESS)
+
+        msg = b"12345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678123456781234567812345678"
+        for _ in range(10):
+            cipher = algsm2.encrypt(msg)
+            self.assertEqual(libsm2.decrypt(cipher), msg)
+
+        for _ in range(10):
+            cipher = libsm2.encrypt(msg)
+            self.assertTrue(algsm2.decrypt(cipher), msg)
+
+    def test_encrypt4(self):
+        sk, pk = lib.sm2.SM2().generate_keypair()
+        libsm2 = lib.sm2.SM2(sk, pk, b"test_encrypt4")
+        msg = b"fast encrypt!fast encrypt!fast encrypt!fast encrypt!fast encrypt!fast encrypt!fast encrypt!fast encrypt!fast encrypt!fast encrypt!fast encrypt!"
+
+        for _ in range(10):
+            self.assertTrue(libsm2.decrypt(libsm2.encrypt(msg)))
 
 
 if __name__ == "__main__":
