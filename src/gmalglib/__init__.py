5c8d92d1f71fd34b5c537dc273ffee1c1fc813c6
ww-rm <ww-rm@qq.com>
2024-04-23 21:15:02 +0800

    update to v0.5.3

--- src/gmalglib/__init__.py
+++ src/gmalglib/__init__.py
@@ -1,7 +1,17 @@
+"""国密算法库
+
+Raises:
+    OverflowError: 数据长度超出范围限制.
+    ValueError: 参数取值不正确.
+    RuntimeError: 运行错误, 例如随机数生成失败.
+    AttributeError: 缺少某些必需参数.
+    MemoryError: 内存不足.
+"""
+
 from . import sm2
 from . import sm3
 from . import sm4
 from . import zuc
 from .wrapped import *
 
-__version__ = "0.5.2"
+__version__ = "0.5.3"
