49359a3e2b0b0ade9ab5adb35bddf8274d444698
ww-rm <ww-rm@qq.com>
2024-05-22 23:28:53 +0800

    add sm9 vsproject

---
+++ include/gmalglib/sm9curve.h
@@ -0,0 +1,15 @@
+#ifndef GMALGLIB_SM9CURVE_H
+#define GMALGLIB_SM9CURVE_H
+
+#include <stdint.h>
+#include <gmalglib/bignum.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // !GMALGLIB_SM9CURVE_H
---
+++ src/gmalglib/core/sm9curve.c
@@ -0,0 +1,3 @@
+#include <stdint.h>
+#include <gmalglib/bignum.h>
+#include <gmalglib/sm9curve.h>
